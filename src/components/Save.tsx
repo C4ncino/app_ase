bbada84d4f0e365838ce844aeacea60672f0ce9d
jose0rtega <josehdez3105@gmail.com>
2024-10-08 17:58:00 -0600

    feat: test file explorer

--- src/components/Save.tsx
+++ src/components/Save.tsx
@@ -10,7 +10,26 @@ interface Props {
 const SaveBase64ToFile = ({ base64Data, fileName }: Props) => {
   const saveFile = async () => {
     // El directorio de documentos donde se guardará el archivo
-    const path = `${FileSystem.documentDirectory}${fileName}`;
+    const dirPath = `${FileSystem.documentDirectory}my_files/`;
+
+    try {
+      // Crear el directorio si no existe
+      const dirInfo = await FileSystem.getInfoAsync(dirPath);
+      if (!dirInfo.exists) {
+        await FileSystem.makeDirectoryAsync(dirPath, { intermediates: true });
+        Alert.alert(
+          "Directorio creado",
+          `El directorio my_files fue creado en: ${dirPath}`
+        );
+      } else {
+        Alert.alert("Directorio ya existe", "El directorio my_files ya existe");
+      }
+    } catch (error) {
+      Alert.alert("Error", "No se pudo crear el directorio");
+      console.error(error);
+    }
+
+    const path = `${FileSystem.documentDirectory}my_files/${fileName}`;
 
     try {
       // Escribir el archivo
@@ -23,10 +42,28 @@ const SaveBase64ToFile = ({ base64Data, fileName }: Props) => {
       console.error(error);
     }
   };
-
+  const listFiles = async () => {
+    try {
+      // Leer el contenido del directorio de documentos
+      const filesList = await FileSystem.readDirectoryAsync(
+        `${FileSystem.documentDirectory}my_files/`
+      );
+      filesList.map((file) => {
+        console.log(file);
+      });
+      Alert.alert(
+        "Archivos listados",
+        "Archivos en el directorio listados correctamente."
+      );
+    } catch (error) {
+      Alert.alert("Error", "No se pudo leer el directorio");
+      console.error(error);
+    }
+  };
   return (
     <View>
       <Button title="Guardar archivo" onPress={saveFile} />
+      <Button title="Listar archivos" onPress={listFiles} />
     </View>
   );
 };
