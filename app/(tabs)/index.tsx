0e1c3c872f62c5b71051ebcaa217ce62a850fb5c
jose0rtega <josehdez3105@gmail.com>
2024-10-14 22:00:15 -0600

    feat: test filesystem

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,12 @@
-import { View, Text, Pressable } from "react-native";
+import { View, Text, Pressable, Button } from "react-native";
 import { useSessionContext } from "@/hooks/useSessionContext";
 import { router } from "expo-router";
 import TranslateSVG from "@/svgs/Translate";
-import SaveBase64ToFile from "@/components/Save";
+import useModelsFiles from "@/hooks/useFile";
 
 const Home = () => {
   const session = useSessionContext();
+  const { saveFile, readFile } = useModelsFiles();
 
   return (
     <View className="justify-start bg-blue-40 items-center w-full h-full ">
@@ -32,7 +33,28 @@ const Home = () => {
         </Text>
       </Pressable>
 
-      <SaveBase64ToFile base64Data={"aG9sYQ=="} fileName={"hola.txt"} />
+      <Button
+        onPress={async () => {
+          try {
+            await saveFile({ salud: "hola" }, "prueba2.json");
+            console.log("Archivo guardado");
+          } catch (error) {
+            console.error("Error guardando el archivo:", error);
+          }
+        }}
+        title="Guardar"
+      />
+      <Button
+        onPress={async () => {
+          try {
+            const data = await readFile("prueba1.json");
+            console.log("Contenido del archivo:", data);
+          } catch (error) {
+            console.error("Error leyendo el archivo:", error);
+          }
+        }}
+        title="Leer"
+      />
     </View>
   );
 };
