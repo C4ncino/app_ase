bad2943347dffc6037718c4ec96fdcf7f20ee1ec
jose0rtega <josehdez3105@gmail.com>
2024-10-17 20:02:54 -0600

    feat: add tensorflowjs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,8 @@
-import { View, Text, Pressable, Button } from "react-native";
-import { useSessionContext } from "@/hooks/useSessionContext";
+import { View, Text, Pressable } from "react-native";
 import { router } from "expo-router";
 import TranslateSVG from "@/svgs/Translate";
-import useModelsFiles from "@/hooks/useFile";
 
 const Home = () => {
-  const session = useSessionContext();
-  const { saveFile, readFile } = useModelsFiles();
-
   return (
     <View className="justify-start bg-blue-40 items-center w-full h-full ">
       <View
@@ -32,29 +27,6 @@ const Home = () => {
           Iniciar traducción
         </Text>
       </Pressable>
-
-      <Button
-        onPress={async () => {
-          try {
-            await saveFile({ salud: "hola" }, "prueba2.json");
-            console.log("Archivo guardado");
-          } catch (error) {
-            console.error("Error guardando el archivo:", error);
-          }
-        }}
-        title="Guardar"
-      />
-      <Button
-        onPress={async () => {
-          try {
-            const data = await readFile("prueba1.json");
-            console.log("Contenido del archivo:", data);
-          } catch (error) {
-            console.error("Error leyendo el archivo:", error);
-          }
-        }}
-        title="Leer"
-      />
     </View>
   );
 };
